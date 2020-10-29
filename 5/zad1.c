3949c6ec614082a5780e9c18852fad8b5a385f4f
halatanatalia <72550671+halatanatalia@users.noreply.github.com>
2020-10-29 16:23:46 +0100

    Update zad1.c

--- 5/zad1.c
+++ 5/zad1.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int dzia5(int a[])
+{
+  int s=0;
+  for(int i=0;i<5;++i)
+  s+=a[i];
+  return s;
+}
+
+int main() 
+{
+  int tab[5]={1,2,3,4,5};
+  printf("%d", dzia5(tab));
+  return 0;
+}
