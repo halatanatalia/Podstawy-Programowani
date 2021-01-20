11008566dbbe3cabd89cb03692e04dd88f431395
halatanatalia <72550671+halatanatalia@users.noreply.github.com>
2021-01-20 13:43:06 +0100

    Create zad3.c

---
+++ 3/zad3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+int l;
+scanf(" %d ", &l);
+
+if(l == 1 || i == 2)
+printf("Slynal z madrosci");
+if(l == 3) 
+printf("Slynal z czegos innego");
+else
+printf("Wiecej synow matka nie miala!");
+
+return 0;
+}
