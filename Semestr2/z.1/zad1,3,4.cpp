aad0c434e088b8e552a7d1019e7708f515a42dc6
halatanatalia <72550671+halatanatalia@users.noreply.github.com>
2021-03-02 22:59:15 +0100

    Create zad1,3,4.cpp

---
+++ Semestr2/z.1/zad1,3,4.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+
+struct ulamek{
+    int l;
+    int m;
+};
+
+void print(int num) {
+    std::cout << num;
+}
+void print(double num) {
+    std::cout << num;
+}
+void print(ulamek ulamek) {
+    std::cout << ulamek.l << "/" << ulamek.m;
+}
+void print(ulamek&ulamek) {
+    std::cout << ulamek.l << "/" << ulamek.m;
+}
+void print(int tab[], int n){
+    for(int i = 0; i < n; i++) {
+        std::cout << tab[i] << " ";
+    }
+}
+void print(...){
+}
+void print(int (&ar)[]){
+    std::cout << ar;
+}
+void print(std::vector <int> v) {
+    for( size_t i = 0; i < v.size(); i++ )
+        std::cout << v[i];
+}
+std::string upper(std::string text) {
+    for(auto &c:text) {
+        if(c == 'd') {
+            c = 'D';
+        }
+    }
+    return text;
+}
+
+int main() {
+
+    std::vector < int > v;
+    v.push_back( 5 );
+    v.push_back( 2 );
+    v.push_back( 3 );
+    v.push_back( 1 );
+    print(v);
+    
+    return 0;
+
+}
